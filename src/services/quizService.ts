158f93d0fcea7a878d3611de876ccc6144ff41fe
MarikaBBB <mbertelli012@gmail.com>
2024-10-01 14:12:25 +0100

    process quiz answers and calculate results

--- src/services/quizService.ts
+++ src/services/quizService.ts
@@ -1,8 +1,10 @@
 // Core logic for quiz generation, question selection
 
-// imports the json file(s) - do this with Marika's data bases
+// imports the json file(s) from data bases
 import quizzesJson from "../../data/quizzes.json";
-import questionJson from "../../data/questions.json"
+import questionJson from "../../data/questions.json";
+import answersJson from "../../data/answers.json";
+import resultsJson from "../../data/results.json"
 
 // shuffle array
 function shuffledArray(array: any[]) {
@@ -34,6 +36,54 @@ function getRandomQuestions(category: string) {
 
     // Step 4: Shuffle the filtered questions to randomise the selection
     const shuffleQuestions = shuffledArray(selectQuiz);
+}
+
+
+// Function to process user answers and calculate the result
+function processQuizAnswers(userId: number, quizId: number, userAnswers: { questionId: number, answer: string }[]) {
+    let totalCorrect = 0;
+   
+
+// Step 1: Loop through the user's answers
+userAnswers.forEach(userAnswers => {
+    const correctAnswer = answersJson.find(answer => answer.questionId === userAnswers.questionId);
+
+    if(!correctAnswer) {
+        console.log(`No correct answer found for the question ID ${userAnswers.questionId}`);
+        return;
+    }
+
+    // Step 2: Check if the user's answer matches the correct answer
+    if(userAnswers.answer.toLowerCase() === correctAnswer.text.toLocaleLowerCase()) {
+        totalCorrect++;
+    }
+
+});
+
+// Step 3: Calculate the score
+const totalQuestions = userAnswers.length;
+const scorePercentage = (totalCorrect/totalQuestions) * 100;
+
+// Step 4: Store the result in result.json
+const totalResult = resultsJson.findIndex(result => result.userId === userId && result.quizId === quizId);
+if(totalResult !== -1) {
+    resultsJson[totalResult].score = scorePercentage;
+} else {
+    resultsJson.push({
+        id: resultsJson.length + 1,
+        quizId: quizId,
+        userId: userId,
+        score: scorePercentage
+
+    });
+}
+
+return {
+    totalCorrect,
+    totalQuestions,
+    scorePercentage
+};
+
 }
 
 
